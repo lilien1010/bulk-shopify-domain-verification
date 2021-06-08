226552986fc432fa72657b072846ede0a492f47b
Lien Li <liling@apache.org>
2021-06-08 11:56:39 +0800

    in case  drop the last line

--- bulk-shopify-domain-verification.sh
+++ bulk-shopify-domain-verification.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat $1 | while read line
+(cat $1;echo) | while read line
 do
   curl --max-time 15  -s -I ${line} | grep -c shopify | awk -v line="$line" '{if($1>0) print line "\tYES"; else print line "\tNO"; }'
 done
