71a94f72571418fa313267576bc66cb6c6b5b12d
Lien Li <liling@apache.org>
2022-11-25 11:15:50 +0800

    Update bulk-get-shopid.sh
    add a \t into result so that you can copy and paste it into google sheet.

--- bulk-get-shopid.sh
+++ bulk-get-shopid.sh
@@ -2,5 +2,5 @@
 
 (cat $1;echo) | while read line
 do
-  curl --max-time 15  -s -I ${line} | grep "X-ShopId:" | awk -v line="$line"  '{print line,$2}'
+  curl --max-time 15  -s -I ${line} | grep "X-ShopId:" | awk -v line="$line"  '{print line,"\t",$2}'
 done
